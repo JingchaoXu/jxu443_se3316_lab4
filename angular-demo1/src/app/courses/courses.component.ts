3ea7adca22ef6d4549d39a2e0c89f236df6e9d6c
Jingchao Xu <jxu443@uwo.ca>
2017-11-10 15:39:09 +0000

    Now the courses service has worked

--- angular-demo1/src/app/courses/courses.component.ts
+++ angular-demo1/src/app/courses/courses.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CoursesService } from '../courses.service'
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css'],
+  providers: [CoursesService],
   encapsulation: ViewEncapsulation.None
 })
 export class CoursesComponent implements OnInit {
   title = "The title of courses page";
-  courses = ["Course1","Course2", "Course3"];
+  courses;
 
-  constructor() { }
+  constructor(courseService: CoursesService) {
+    
+    this.courses=courseService.getCourses();
+  }
 
   ngOnInit() {
   }
