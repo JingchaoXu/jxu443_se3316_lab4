1e486317a0ef26c1f2c23a7c22f4dfcbcc505d16
Jingchao Xu <jxu443@uwo.ca>
2017-11-12 02:08:51 +0000

    Add the saveCourse function

--- angular-demo1/src/app/courses.service.ts
+++ angular-demo1/src/app/courses.service.ts
@@ -7,5 +7,12 @@ export class CoursesService {
   getCourses() : string[]{
        return ["Course1","Course2", "Course3"];
   }
+  
+  saveCourses(newCourse: string)
+  {
+    this.courses = this.getCourses();
+    this.courses.push(newCourse);
+    
+  }
 
 }
